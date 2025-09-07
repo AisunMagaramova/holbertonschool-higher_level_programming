4f12ad2303dfbd42bda597f8a0f8caf1e23a3024
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-09-07 11:14:12 +0000

    t3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(c) for c in range(97, 123) if chr(c) not in "qe")), end="")
+print("{}".format(
+    "".join(chr(c) for c in range(97, 123) if chr(c) not in "qe")), end="")
