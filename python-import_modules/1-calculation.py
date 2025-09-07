ce3d77ab1f3991c465e14d2e4d012e40bddbc386
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-09-07 12:06:21 +0000

    t1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,51 +1,11 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
+from calculator_1 import add, sub, mul, div
 
-    Args:
-        a: first integer
-        b: second integer
+a = 10
+b = 5
 
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
-
-
-def sub(a, b):
-    """My subtraction function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a - b
-    """
-    return (a - b)
-
-
-def mul(a, b):
-    """My multiplication function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a * b
-    """
-    return (a * b)
-
-
-def div(a, b):
-    """My division function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a / b
-    """
-    return int(a / b)
+if __name__ == "__main__":
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
