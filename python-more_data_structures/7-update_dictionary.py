7d753cd4e8d1e53914bf84cbc40dbb820f48fa90
AisunMagaramova <mahrramovaaysun1122@gmail.com>
2025-09-13 13:28:12 +0000

    t7

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    # Update the key with the new value or add a new key-value pair if the key doesn't exist
+    a_dictionary[key] = value
+    return a_dictionary
