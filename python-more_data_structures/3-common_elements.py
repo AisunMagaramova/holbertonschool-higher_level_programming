cf7e3621f346dc211502741e6b6e493c97600a6c
AisunMagaramova <mahrramovaaysun1122@gmail.com>
2025-09-13 13:08:36 +0000

    t3

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    # Use the intersection operator (&) to find common elements between two sets
-    return set_1 & set_2  # This is already within 79 characters
+    # Use the intersection operator (&) to find common elements
+    # between two sets.
+    return set_1 & set_2
