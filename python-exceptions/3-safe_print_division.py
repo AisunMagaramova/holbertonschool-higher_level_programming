0998480e25c52b5ce9cbc18e836ccea12ae88aec
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-09-14 11:26:42 +0000

    t3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        # Attempt to divide a by b
+        result = a / b
+    except ZeroDivisionError:
+        # Catch division by zero exception
+        result = None
+    finally:
+        # Print the result inside the finally block
+        print("Inside result: {}".format(result))
+    
+    return result
