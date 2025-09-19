c795d40fe3f4171babee6cda4507c63baba7c4cb
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-09-19 14:59:11 +0000

    t1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -11,3 +11,10 @@ class Square:
         :param size, kvadartin olcusu
         """
         self.__size = size
+
+        def size(self):
+            return self.__size
+
+        My_square = Square(3)
+        print(my_square.size)
+        print(my_square.__size)
