238ca91a0e4c616e93882da781032331a4726590
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-09-19 15:10:46 +0000

    t1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
+"""
+Kvadrat sinifi, size atributu ile olcusunu teyin edirik.
+"""
+
 class Square:
-    """
-    Kvadrat sinifi, size atributu ile olcusunu teyin edirik.
-    """
-
-
     def __init__(self, size):
         """
         Yeni kvadrat sinifi yarat.
-        :param size, kvadartin olcusu
+        :param size: kvadratın ölçüsü
         """
         self.__size = size
 
-        def size(self):
-            return self.__size
+    @property
+    def size(self):
+        """
+        size atributunun getter metodu.
+        """
+        return self.__size
 
-        My_square = Square(3)
-        print(my_square.size)
-        print(my_square.__size)
+
+# Sinifdən obyekt yaradılması və çap edilməsi modul səviyyəsində
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(my_square.size)        # 3
+    # print(my_square.__size)    # Bu xəta verər, gizli atributa birbaşa müraciət olmaz
