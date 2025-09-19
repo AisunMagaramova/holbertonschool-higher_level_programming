f7c24cdbf8beb96811832d7751054c52ecd435b7
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-09-19 14:25:56 +0000

    t1

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/use/bin/python3
+class Square:
+    """
+    Kvadrat sinifi, size atributu ile olcusunu teyin edirik.
+    """
+
+
+    def __init__(self, size):
+        """
+        Yeni kvadrat sinifi yarat.
+        :param size, kvadartin olcusu
+        """
+        self.size = size
