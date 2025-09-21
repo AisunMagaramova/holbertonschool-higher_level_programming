8c5e8be5e2e32c394c5f8157064a8bfcbaea54f0
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-09-21 08:20:43 +0000

    t1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,11 +2,9 @@
 """ Squate classini teyin eden modul."""
 
 
-
 class Square:
     """ Kvadrat teyin edilir. """
 
-    
-    def __init__(self, size):
+     def __init__(self, size):
         """ Pirivate size ilile kvadrat ise salinir. """
         self.__size = size
