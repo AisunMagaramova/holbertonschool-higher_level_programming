d4a7c3e3c3a7aaed4404e4604dcbb06388c753dd
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-09-27 18:01:50 +0400

    t9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -34,6 +34,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
+
 class Rectangle(BaseGeometry):
     """
     A class representing a rectangle, which inherits from BaseGeometry
