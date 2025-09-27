b0f85e4d1785a65ffe560675b2e1889a3ed50d0f
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-09-27 15:59:01 +0400

    t5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+"""
+This module contains a function 'is_same_class' that checks if an object
+is exactly an instance of the specified class.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometry-related shapes and operations.
+    This class is empty for now.
+    """
+    pass
+    
