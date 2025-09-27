0f6408cca3c3d178412e45776762e7633f27a122
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-09-27 16:53:03 +0400

    t8

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module contains a class 'Rectangle' that represents a rectangle.
+It inherits from BaseGeometry.
+"""
 
 class BaseGeometry:
     """
@@ -29,3 +33,34 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """
+    A class that represents a rectangle, inherits from BaseGeometry.
+    Attributes:
+    width (int): The width of the rectangle.
+    height (int): The height of the rectangle.
+    Methods:
+    area(): Returns the area of the rectangle.
+    """
+    
+    def __init__(self, width, height):
+        """
+        Initializes the rectangle with width and height. Both attributes are validated
+        to be positive integers.
+        Parameters:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height 
+    
+    def area(self):
+        """
+        Returns the area of the rectangle.
+        Returns:
+        int: The area of the rectangle (width * height).
+        """
+        return self.__width * self.__height
