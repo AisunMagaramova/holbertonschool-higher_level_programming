52ead610f6c88802aed178cbb095f903a2957a72
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-09-27 15:21:38 +0400

    t2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def is_same_class(obj, a_class):
-""" JRGH3 IJG 4OJG OG. """
+""" Check if the object is exactly an instance of the specified class."""
 return type(obj) is a_class
