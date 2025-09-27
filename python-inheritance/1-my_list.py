ee7039851d5b91fc822ed1f3dd6fd4f34ca8eddd
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-09-27 14:48:22 +0400

    t2

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""
+This module contains a function that returns the list of available
+attributes and methods of an object.
+
+It defines the function 'lookup'.
+"""
+
+class MyList(list)
+   def print_sorted(self):
+    
+    ptint (sorted(self))
