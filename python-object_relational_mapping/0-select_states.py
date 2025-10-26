d141adb6d934a1d18052b21366241ab874610789
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-10-26 14:12:46 +0000

    t0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,8 +9,7 @@ from sys import argv
 
 if __name__ == "__main__":
     # Connect to the database
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
     
     # Get all states ordered by id
