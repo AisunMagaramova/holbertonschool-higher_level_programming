88bbbed00ff3b6e74cc8acb76a596d3f21187f8c
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-10-26 11:06:37 +0000

    t16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,7 +1,7 @@
 -- cedveldeki buutun qeydleri sadalyiriq
 -- nma e sutununds dyer olmayuan yeni NULL OLANLARI SAADALAMA
 -- NAME VE SCORE SADA (ORDER ILE), AZALAN SIRA ILE(DECS)
-SELECT name, score
+SELECT score, name
 FROM second_table
 WHERE 'name' IS NOT NULL
 ORDER BY score DESC;
