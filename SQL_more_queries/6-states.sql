a5c9e5df2869ff171ab45ff224fc0c0037da4b2f
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-10-26 13:44:02 +0000

    t6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- hbtn_0d_usa data base yaradiri
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXSIST `hbtn_0d_usa`.`states` (
+CREATE TABLE IF NOT EXSIST hbtn_0d_usa.states (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL
 	);
