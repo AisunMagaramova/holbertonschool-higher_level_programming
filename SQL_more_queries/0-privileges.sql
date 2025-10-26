a4a0b7011c13bbd8759708fa41dfcf2f9f470860
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-10-26 12:53:17 +0000

    t0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- user_0d_1 ve user_0d_2 istifadecisinin butuin imtiyazlalrini sadaLA 
-SHOW GRANTS FOR 'user_0d_1'@'localhost'
-SHOW GRANTS FOR '0user_0d_2'@'localhost'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR '0user_0d_2'@'localhost';
